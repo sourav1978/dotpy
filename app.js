ee316ee209ee7f5b609445b86c30709873538bc0
Sourav Banerjee <souravbanerjee1978@gmail.com>
2019-02-02 12:54:56 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+
+fs.readdir('./', function( err,files ){
+    if(err) console.log('Error:',err);
+    else console.log('Result: ',files);
+})
+//const path = require('path');
+// const os = require('os');
+// var totalMem    = os.totalmem();
+// var freeMem = os.freemem();
+
+// console.log(`Total Memory: ${totalMem}`);
+// console.log(`Free Memory: ${freeMem}` );
+//console.log(path);
+//var pathObj = path.parse(__filename);
+//console.log(pathObj);
+//var log = require('./logger');
+//log(" Sourav");
+//console.log(module);
+
+// function sayHello(name){
+//     console.log("Hello "+name)
+// }
+// sayHello('Sourav')
